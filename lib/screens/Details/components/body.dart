83d3525325dee50a231fba34a541f94f18aa4639
WCFX <wcfx.dev@gmail.com>
2021-05-31 01:36:58 -0300

    feat: backdrop da pagina details criado

--- lib/screens/Details/components/body.dart
+++ lib/screens/Details/components/body.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:movie_info/models/movie.dart';
 
 class Body extends StatelessWidget {
+  final Movie movie;
+
+  const Body({Key key, this.movie}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Text('Olá Marilene'),
+        Container(
+          height: size.height * 0.4,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.4 - 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                  ),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(movie.backdrop),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
--- lib/screens/Details/details_screen.dart
+++ lib/screens/Details/details_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:movie_info/models/movie.dart';
 import 'package:movie_info/screens/Details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
+  final Movie movie;
+
+  const DetailsScreen({Key key, this.movie}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Body(),
+      body: Body(movie: movie),
     );
   }
 }
