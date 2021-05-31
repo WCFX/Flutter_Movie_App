e0e40e9e46fca770abcdc008735b719416c38780
WCFX <wcfx.dev@gmail.com>
2021-05-31 00:55:48 -0300

    feat: inicio criacao tela de detalhes

---
+++ lib/screens/Details/details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:movie_info/screens/Details/components/body.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Body(),
+    );
+  }
+}
